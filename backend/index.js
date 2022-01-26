e86c2b530a6883d755f711aee48c4106dd5149d9
solenedel <solene98@student.ubc.ca>
2022-01-26 12:06:11 -0800

    add start script for backend & install nodemon

--- backend/index.js
+++ backend/index.js
@@ -3,3 +3,10 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 require("dotenv").config();
+
+const app = express();
+
+// -------------------------------------------------- //
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`);
+});
