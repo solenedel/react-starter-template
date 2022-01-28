8e100f8bf35b3af83fa12c684f0d94592c93e429
solenedel <solene98@student.ubc.ca>
2022-01-27 17:34:06 -0800

    display results of GET request in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Dashboard from "./components/Dashboard";
 
 function App() {
+  const [userData, setUserData] = useState([]);
   // test getting data from database
   useEffect(() => {
     axios
       .get("http://localhost:8081/dbtest")
       .then((response) => {
-        console.log("DATA FROM DATABASE", response.data);
+        // console.log("DATA FROM DATABASE", response.data[0]);
+        setUserData((prev) => [...prev, ...response.data]);
       })
       .catch((err) => console.error(err));
   }, []);
 
   return (
     <div className="App">
-      <h3>My react project</h3>
       <Dashboard />
+      <div>
+        {userData.length &&
+          userData.map((user) => {
+            return (
+              <React.Fragment key={user.id}>
+                <div>Username: {user.username}</div>
+                <div>email: {user.email}</div>
+              </React.Fragment>
+            );
+          })}
+      </div>
     </div>
   );
 }
